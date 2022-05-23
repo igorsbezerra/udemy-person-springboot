5826dc11b0bec1afb0e42c557dca71cff95fd36f
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-05-23 19:56:34 -0300

    feat: Controller Person

---
+++ src/main/java/com/udemy/person/controller/PersonController.java
@@ -0,0 +1,44 @@
+package com.udemy.person.controller;
+
+import com.udemy.person.data.vo.PersonVO;
+import com.udemy.person.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonService personService;
+
+    @PostMapping
+    public ResponseEntity<PersonVO> create(@RequestBody PersonVO person) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(personService.create(person));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PersonVO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(personService.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PersonVO>> findAll() {
+        return ResponseEntity.ok(personService.findAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PersonVO> update(@PathVariable Long id, @RequestBody PersonVO person) {
+        return ResponseEntity.ok(personService.update(id, person));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Long id) {
+        personService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
