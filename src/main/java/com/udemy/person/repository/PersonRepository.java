8afd36cc88c0eca6c33f9895706f77645c266965
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-05-23 19:54:58 -0300

    feat: Person repository with JpaRepository

---
+++ src/main/java/com/udemy/person/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.udemy.person.repository;
+
+import com.udemy.person.data.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
