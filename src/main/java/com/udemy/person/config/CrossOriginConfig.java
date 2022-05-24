5d5d631fb6d13762ce83b42a1959737f5dc8b010
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-05-24 15:19:01 -0300

    config: enable cross origin

---
+++ src/main/java/com/udemy/person/config/CrossOriginConfig.java
@@ -0,0 +1,14 @@
+package com.udemy.person.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CrossOriginConfig implements WebMvcConfigurer {
+    public void addCorsMapping(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
