4efa66399c71670f080ebe6198d558bc519567d0
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-05-23 22:26:54 -0300

    feat: migrations to database

---
+++ src/main/resources/db/migration/V2__POPULATE_TABLE_PERSON.sql
@@ -0,0 +1,4 @@
+INSERT INTO `tb_person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+    (1, 'São Carlos-SP', 'Igor de Souza', 'Male', 'Bezerra'),
+    (2, 'São Carlos-SP', 'Yuri de Souza', 'Male', 'Bezerra'),
+    (3, 'São Carlos-SP', 'Lívia Karoline', 'Female', 'Freitas');
