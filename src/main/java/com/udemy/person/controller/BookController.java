3f9552706e1f1a8e5bc87d67305451cc2fecad8e
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-05-24 14:32:53 -0300

    doc: add documentation to end points

--- src/main/java/com/udemy/person/controller/BookController.java
+++ src/main/java/com/udemy/person/controller/BookController.java
@@ -2,6 +2,8 @@ package com.udemy.person.controller;
 
 import com.udemy.person.data.vo.BookVO;
 import com.udemy.person.services.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,31 +14,37 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/book")
+@Tag(name = "Book", description = "End Points to Book")
 public class BookController {
 
     @Autowired
     private BookService bookService;
 
+    @Operation(summary = "Create a book")
     @GetMapping
     public ResponseEntity<List<BookVO>> findAll() {
         return ResponseEntity.ok().body(bookService.findAll());
     }
 
+    @Operation(summary = "Find one by id")
     @GetMapping("/{id}")
     public ResponseEntity<BookVO> findOne(@PathVariable Long id) {
         return ResponseEntity.ok().body(bookService.findById(id));
     }
 
+    @Operation(summary = "List all books")
     @PostMapping
     public ResponseEntity<BookVO> create(@RequestBody BookVO bookVO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(bookService.create(bookVO));
     }
 
+    @Operation(summary = "Update a book")
     @PutMapping("/{id}")
     public ResponseEntity<BookVO> update(@PathVariable Long id, @RequestBody BookVO bookVO) {
         return ResponseEntity.ok().body(bookService.update(id, bookVO));
     }
 
+    @Operation(summary = "Delete one book")
     @DeleteMapping("/{id}")
     public ResponseEntity delete(@PathVariable Long id) {
         bookService.delete(id);
--- src/main/java/com/udemy/person/controller/PersonController.java
+++ src/main/java/com/udemy/person/controller/PersonController.java
@@ -2,6 +2,8 @@ package com.udemy.person.controller;
 
 import com.udemy.person.data.vo.PersonVO;
 import com.udemy.person.services.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,31 +13,37 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/person")
+@Tag(name = "Person", description = "End Points to Person")
 public class PersonController {
 
     @Autowired
     private PersonService personService;
 
+    @Operation(summary = "Create a person")
     @PostMapping
     public ResponseEntity<PersonVO> create(@RequestBody PersonVO person) {
         return ResponseEntity.status(HttpStatus.CREATED).body(personService.create(person));
     }
 
+    @Operation(summary = "Find one by id")
     @GetMapping("/{id}")
     public ResponseEntity<PersonVO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(personService.findById(id));
     }
 
+    @Operation(summary = "List all people")
     @GetMapping
     public ResponseEntity<List<PersonVO>> findAll() {
         return ResponseEntity.ok(personService.findAll());
     }
 
+    @Operation(summary = "Update a person")
     @PutMapping("/{id}")
     public ResponseEntity<PersonVO> update(@PathVariable Long id, @RequestBody PersonVO person) {
         return ResponseEntity.ok(personService.update(id, person));
     }
 
+    @Operation(summary = "Delete one person")
     @DeleteMapping("/{id}")
     public ResponseEntity delete(@PathVariable Long id) {
         personService.delete(id);
