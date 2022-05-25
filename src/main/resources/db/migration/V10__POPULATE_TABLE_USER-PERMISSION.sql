8b1373da11f2e5f23e23ba27e8891737bf6f3b70
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-05-24 21:50:33 -0300

    feat: prepare database with spring security

---
+++ src/main/resources/db/migration/V10__POPULATE_TABLE_USER-PERMISSION.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO `tb_user_permission` (`id_user`, `id_permission`) VALUES
+	(1, 1),
+	(2, 1),
+	(1, 2);
---
+++ src/main/resources/db/migration/V6__POPULATE_TABLE_PERMISSION.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO `tb_permission` (`description`) VALUES
+	('ADMIN'),
+	('MANAGER'),
+	('COMMON_USER');
